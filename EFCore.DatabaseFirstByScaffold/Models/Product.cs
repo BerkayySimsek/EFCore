9b70533c262697198c7ad6ea98f2c0e195d6826c
BerkaySimsek <berkayysimsekk@gmail.com>
2025-03-19 01:09:56 +0300

    Scaffold Added

---
+++ EFCore.DatabaseFirstByScaffold/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace UdemyEFCore.DatabaseFirstByScaffold.Models
+{
+    public partial class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
+    }
+}
---
+++ EFCore.DatabaseFirstByScaffold/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using UdemyEFCore.DatabaseFirstByScaffold.Models;
+
+
+
+using (var context = new EFCoreDatabaseFırstDbContext())
+{
+    var products = await context.Products.ToListAsync();
+
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id} :{p.Name} - {p.Price}");
+    });
+}
