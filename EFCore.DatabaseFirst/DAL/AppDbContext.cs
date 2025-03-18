93d3a3cd7b89450f9a4c4c34e529b3daaf5fac53
BerkaySimsek <berkayysimsekk@gmail.com>
2025-03-19 00:30:15 +0300

    Add project files.

---
+++ EFCore.DatabaseFirst/DAL/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using UdemyEFCore.DatabaseFirst.DAL;
+
+namespace EFCore.DatabaseFirst.DAL;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Product>? Products { get; set; }
+    public AppDbContext()
+    {
+        
+    }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(DbContextInitializer.Configuration.GetConnectionString("SqlCon"));
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
